27195c4fa0f2cc54a1dc46e42bbf658021532f4e
Ni WANG <ni.wang@rea-group.com>
2014-11-28 00:12:57 +0800

    rack call

--- awesome-web-app.rb
+++ awesome-web-app.rb
@@ -1,3 +1,9 @@
 require 'rack'
 
-Rack::Handler::WEBrick.run Proc.new {|env| ['200', {'Content-Type' => 'text/html'}, ["Hello Rack!"]]}, :Port => 9898
+class HelloRack
+  def call(env)
+    ['200', {'Content-Type' => 'text/html'}, ["Hello Rack!"]]
+  end
+end
+
+Rack::Handler::WEBrick.run HelloRack.new, :Port => 9898
